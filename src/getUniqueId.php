32ec1c31d72b8d05c7c3ac1700e599a259729298
jorgeuos <jorge.powers@digitalistgroup.com>
2022-04-29 12:00:39 +0200

    Getting weird behaviour wihtin client

--- src/getUniqueId.php
+++ src/getUniqueId.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // use Jane\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
 use Jane\Component\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
+// use Jane\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
 use GuzzleHttp\Client;
 use Digitalist\Library\UniqueID\Authentication\ApiKeyAuthentication;
 use Digitalist\Library\UniqueID\Client as GeneratedClient;
@@ -17,15 +18,23 @@ class SDGClient extends GeneratedClient {
     public static function create($httpClient = null, array $additionalPlugins = array(), array $additionalNormalizers = array()) {
         $apiKey = getenv('SDGAPIKEY');
         $authenticationRegistry = new AuthenticationRegistry([new ApiKeyAuthentication($apiKey)]);
+        // var_dump($authenticationRegistry);
+
         $client = new Client([
-            'base_uri' => 'https://collect.sdgacceptance.eu/v1/',
+            'base_uri' => 'https://collect.sdgacceptance.eu/v1',
             // 'base_uri' => 'https://collect.youreurope.ec.europa.eu/v1/',
         ]);
         $httpClient = new \Http\Client\Common\PluginClient($client, [$authenticationRegistry]);
         return parent::create($httpClient);
     }
 }
-
+/**
+ * Should call API like this:
+ * curl --location --request GET 'https://collect.sdgacceptance.eu/v1/unique-id' \
+ * --header 'x-api-key: SExxxxXXXXXxxxxxXXXXxxxx'
+ * "dddd44s-eeee-4b5b-a2a2-ede7400005a0-1630000000000"
+ */
 
 $apiClient = SDGClient::create();
-var_dump($apiClient->getUniqueId());
+$resp = $apiClient->getUniqueId();
+var_dump($resp);
